c6c8b1088a7a4748f74a3cd209e951a8665c8539
Darlan <darlansilv4@gmail.com>
2023-02-27 21:53:40 -0300

    Iniciando verificações no Model de Alunos

--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -3,9 +3,32 @@ import Sequelize, { Model } from 'sequelize';
 export default class Aluno extends Model {
   static init(sequelize) {
     super.init({
-      name: Sequelize.STRING,
-      lastname: Sequelize.STRING,
-      email: Sequelize.STRING,
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'O campo nome deve ter entre 3 e 255 caracteres',
+          },
+        },
+      },
+      lastname: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Já exite um aluno com esse email',
+        },
+        validate: {
+          isEmail: {
+            msg: 'Email inválido',
+          },
+        },
+      },
       age: Sequelize.INTEGER,
       weight: Sequelize.FLOAT,
       height: Sequelize.FLOAT,
