896347a1ebd3b536c238af3819578dd28bb4b586
Darlan <darlansilv4@gmail.com>
2023-03-06 21:43:55 -0300

    Alterações no build

--- dist/config/app.js
+++ dist/config/app.js
@@ -1,3 +1,3 @@
 "use strict";module.exports = {
-  url: 'http://34.95.169.95',
+  url: process.env.APP_URL,
 };
--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,4 @@
 "use strict"; function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _app = require('./app'); var _app2 = _interopRequireDefault(_app);
 
-const port = 5000;
-_app2.default.listen(port, () => {
-  console.log();
-  console.log(`Escutando na porta ${port}`);
-  console.log(`CTRL + Clique http://localhost:${port}`);
-});
+const port = process.env.APP_PORT;
+_app2.default.listen(port);
